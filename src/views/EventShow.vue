162309a326ec0463316649af6b7fb8f73e5d983d
barein <nicolas.bareil@outlook.com>
2019-08-05 21:11:04 +0200

    Progress bar with in-component route guard

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -3,15 +3,18 @@
     <div class="event-header">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h1 class="title">{{ event.title }}</h1>
-      <h5>Organized by {{ event.organizer ? event.organizer.name : ''}}</h5>
+      <h5>Organized by {{ event.organizer ? event.organizer.name : "" }}</h5>
       <h5>Category: {{ event.category }}</h5>
     </div>
     <BaseIcon name="map"><h2>Location</h2></BaseIcon>
     <address>{{ event.location }}</address>
     <h2>Event details</h2>
     <p>{{ event.description }}</p>
-    <h2>Attendees
-      <span class="badge -fill-gradient">{{ event.attendees ? event.attendees.length : 0 }}</span>
+    <h2>
+      Attendees
+      <span class="badge -fill-gradient">{{
+        event.attendees ? event.attendees.length : 0
+      }}</span>
     </h2>
     <ul class="list-group">
       <li
@@ -27,40 +30,48 @@
 
 <script>
 import { mapState } from "vuex";
+import nProgress from "nprogress";
+import store from "@/store/store";
 
 export default {
   props: ["id"],
   data() {
     return {};
   },
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    nProgress.start();
+
+    store.dispatch("eventModule/fetchEvent", routeTo.params.id).then(() => {
+      nProgress.done();
+
+      next();
+    });
+  },
   computed: {
     ...mapState({
       event: state => state.eventModule.event
     })
-  },
-  created() {
-    this.$store.dispatch("eventModule/fetchEvent", this.id);
   }
 };
 </script>
 
 <style scoped>
-  .location {
-    margin-bottom: 0;
-  }
-  .location > .icon {
-    margin-left: 10px;
-  }
-  .event-header > .title {
-    margin: 0;
-  }
-  .list-group {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-  }
-  .list-group > .list-item {
-    padding: 1em 0;
-    border-bottom: solid 1px #e5e5e5;
-  }
+.location {
+  margin-bottom: 0;
+}
+.location > .icon {
+  margin-left: 10px;
+}
+.event-header > .title {
+  margin: 0;
+}
+.list-group {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.list-group > .list-item {
+  padding: 1em 0;
+  border-bottom: solid 1px #e5e5e5;
+}
 </style>
